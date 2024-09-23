e111dba9d60c6162e4ee5ba7827389fc165f9837
Fabio Lobo <contato@fabiolobo.com.br>
2024-09-23 18:30:05 -0300

    Opção de download de nota

--- resources/views/painel/notas/show.blade.php
+++ resources/views/painel/notas/show.blade.php
@@ -6,7 +6,7 @@
 	<header class="heading">
 		<h1 class="title title--section title--small">
 			@if (isset($nota->projeto) && $nota->projeto)
-				<a href="{{ route('projetos.show', $nota->projeto->id) }}">{{ $nota->projeto->nome }}</a> › 
+				<a href="{{ route('projetos.show', $nota->projeto->id) }}">{{ $nota->projeto->nome }}</a> ›
 			@endif
 			Editar anotação
 		</h1>
@@ -26,8 +26,45 @@
 	</header>
 
 	<section class="main__section">
-		<div class="quill-result box">
+		<div class="quill-result box" id="quillContent">
 			{!! $nota->nota !!}
+		</div>
+
+		<div class="actions">
+			<button class="actions__button" id="downloadPdfButton">
+				<?php \App\Helpers\SvgHelper::render(['name' => 'download', 'class' => 'center']); ?>
+				<span class="actions__button__text">Baixar PDF</span>
+			</button>
 		</div>
 	</section>
 @endsection
+
+@push('scripts')
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.3.8/purify.min.js"></script>
+
+	<script>
+		document.getElementById('downloadPdfButton').addEventListener('click', function () {
+			const { jsPDF } = window.jspdf;
+			const doc = new jsPDF({
+				unit: 'pt',
+				format: 'a4'
+			});
+
+			let notaNome = "{{ $nota->nome }}".replace(/\s+/g, '_').toLowerCase();
+			let content = document.getElementById('quillContent').innerHTML;
+
+			doc.html(content, {
+				callback: function (doc) {
+					doc.save(`${notaNome}.pdf`);
+				},
+				x: 20,
+				y: 20,
+				width: 555,
+				windowWidth: 800,
+				autoPaging: true
+			});
+		});
+	</script>
+@endpush
